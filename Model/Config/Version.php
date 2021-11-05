705d458eb003444e586bdd7cb1495f93bc4bae19
martin <magtec@gmail.com>
2021-11-05 10:32:19 -0300

    adding test cases

--- Model/Config/Version.php
+++ Model/Config/Version.php
@@ -11,6 +11,7 @@ use Magento\Framework\Model\ResourceModel\AbstractResource;
 use Magento\Framework\Data\Collection\AbstractDb;
 use Magento\Framework\Module\ModuleList;
 use Magento\Framework\Module\Manager;
+use Magento\Framework\Exception\LocalizedException;
 
 class Version extends \Magento\Framework\App\Config\Value
 {
@@ -85,13 +86,17 @@ class Version extends \Magento\Framework\App\Config\Value
 
     /**
      * @return string
+     * @throws LocalizedException
      * @throws \Safe\Exceptions\JsonException
      */
     protected function getCustomModules()
     {
         $result = [];
-
         $modules = $this->moduleList->getNames();
+        if ($modules === null) {
+            throw new LocalizedException(__('Could not get module list.'));
+        }
+
         foreach ($modules as $module) {
             if (strpos($module, self::MTOOLS_VENDOR) !== false) {
                 $result[] = [
--- Test/Unit/Model/Config/VersionTest.php
+++ Test/Unit/Model/Config/VersionTest.php
@@ -3,7 +3,9 @@
 namespace Mtools\Core\Test\Unit\Model\Config;
 
 use PHPUnit\Framework\TestCase;
+use Safe\Exceptions\JsonException;
 use Mtools\Core\Model\Config\Version;
+use Magento\Framework\Exception\LocalizedException;
 
 class VersionTest extends TestCase
 {
@@ -56,10 +58,43 @@ class VersionTest extends TestCase
 
     /**
      * @test
+     */
+    public function testAfterLoad()
+    {
+        $this->doesNotPerformAssertions();
+        $this->moduleList->method('getNames')
+            ->willReturn([]);
+        $this->versionObject->afterLoad();
+    }
+
+    /**
+     * @test
+     */
+    public function testAfterLoadLocalizedException()
+    {
+        $this->expectException(LocalizedException::class);
+        $this->expectExceptionMessage('Could not get module list.');
+        $this->versionObject->afterLoad();
+    }
+
+    /**
+     * @test
+     */
+    public function testAfterLoadJsonException()
+    {
+        $this->expectException(JsonException::class);
+        $this->moduleList->method('getNames')->willReturn(['Mtools_Core']);
+        $this->moduleResource->method('getDbVersion')->willReturn("\xB1\x31");
+        $this->moduleManager->method('isEnabled')->willReturn(1);
+        $this->versionObject->afterLoad();
+    }
+
+    /**
+     * @test
      * @dataProvider dataProvider
      * @throws \ReflectionException
      */
-    public function testGetCustomValues($list, $version, $status, $expected)
+    public function testGetCustomValues($list, $status, $expected)
     {
         $method = $this->versionReflection->getMethod('getCustomModules');
         $method->setAccessible(true);
@@ -68,7 +103,7 @@ class VersionTest extends TestCase
             ->willReturn($list);
 
         $this->moduleResource->method('getDbVersion')
-            ->willReturn($version);
+            ->willReturn($this->returnCallback([$this,'getDbVersionCallback']));
 
         $this->moduleManager->method('isEnabled')
             ->willReturn($status);
@@ -77,33 +112,47 @@ class VersionTest extends TestCase
         $this->assertEquals($getCustomModules, $expected);
     }
 
+    /**
+     * @return array[]
+     */
     public function dataProvider()
     {
         return [
             'Empty module list' => [ /* test case description */
-                [], '1.2.0', 1, '[]' /* params: list, version, status, expected_result */
+                [], 1, '[]' /* params: list, status, expected_result */
             ],
             'Module list without matches' => [
-                ['Test_Customer','Test_Cms'], '1.2.0', 1, '[]'
+                ['Test_Customer','Test_Cms'], 1, '[]'
             ],
             'Module list with match enabled' => [
                 ['Test_Customer','Test_Cms','Mtools_Core'],
-                '1.2.0',
                 1,
                 '[{"name":"Mtools_Core","version":"1.2.0","active":1}]'
             ],
             'Module list with match disabled' => [
                 ['Test_Customer','Test_Cms','Mtools_Core'],
-                '1.2.0',
                 0,
                 '[{"name":"Mtools_Core","version":"1.2.0","active":0}]'
             ],
             'Module list with multiple matches' => [
                 ['Test_Customer','Test_Cms','Mtools_Core','Mtools_CronRun'],
-                '1.2.0',
                 1,
                 '[{"name":"Mtools_Core","version":"1.2.0","active":1},{"name":"Mtools_CronRun","version":"1.2.0","active":1}]'
             ]
         ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getDbVersionCallback()
+    {
+        /* The purpose of this callback is just to implement some logic in a mocked method for documentation */
+        $args = func_get_args();
+        $version = '1.2.0';
+        if (!is_string($args[0])) {
+            $version .= '.1';
+        }
+        return $version;
     }
 }
